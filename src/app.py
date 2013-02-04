8d2cf524aacdd413dd8b99f9fe2b0f3592c3fa05
Egor Popov <popovegor@gmail.com>
2013-02-04 18:15:34 +0400

    add root path in app.py

--- src/app.py
+++ src/app.py
@@ -8,6 +8,10 @@ import logging
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['GET'])
+def defualt():
+    return "http log viewer"
+
 @app.route('/log', methods = ['GET'])
 def log():
     query_path = request.args.get("path") 
